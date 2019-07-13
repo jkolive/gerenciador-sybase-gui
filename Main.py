bb1d227e5e60574d11fd24d17996698bbbeff4d4
Jackson de Oliveira <mr.jkolive@gmail.com>
2019-07-12 22:51:02 -0300

    Corrigindo erro: GtkDialog mapped without a transient parent

--- Main.py
+++ Main.py
@@ -4,14 +4,18 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 
-class Main:
+class Main(Gtk.Window):
     def __init__(self):
+        Gtk.Window.__init__(self)
         builder = Gtk.Builder()
         builder.add_from_file('App.glade')
 
         self.list_store = builder.get_object('list_store')
         self.list_store.append([True, " /home/jackson/Dados", "Contabil.db"])
         self.treeview = builder.get_object('treeView')
+
+        btn_file = Gtk.Button()
+        btn_file.connect("clicked", self.on_btn_file_clicked)
 
         renderer_toggle = Gtk.CellRendererToggle()
         renderer_toggle.connect("toggled", self.on_cbox_db_toggled)
@@ -27,43 +31,41 @@ class Main:
         column_text = Gtk.TreeViewColumn("Nome", renderer_text, text=2)
         self.treeview.append_column(column_text)
 
-        builder.connect_signals(Handlers)
-        window = builder.get_object("window_main")
-        window.show_all()
+        builder.connect_signals(self)
+        self.window = builder.get_object("window_main")
+        self.window.show_all()
 
     def on_cbox_db_toggled(self, widget, path):
         self.list_store[path][0] = not self.list_store[path][0]
 
+    def on_btn_file_clicked(self, button):
+        dialog = Gtk.FileChooserDialog("Localize o banco de dados", self, Gtk.FileChooserAction.OPEN,
+                                       (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
 
-def open_file(button):
-    dialog = Gtk.FileChooserDialog("Localize o banco de dados", None, Gtk.FileChooserAction.OPEN,
-                                   (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+        self.add_filters(dialog)
+        dialog.set_transient_for(self)
+        response = dialog.run()
 
-    add_filters(dialog)
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            # print("File selected: " + dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
 
-    response = dialog.run()
+        dialog.destroy()
 
-    if response == Gtk.ResponseType.OK:
-        print("Open clicked")
-        # print("File selected: " + dialog.get_filename())
-    elif response == Gtk.ResponseType.CANCEL:
-        print("Cancel clicked")
+    def add_filters(self, dialog):
+        filter_file = Gtk.FileFilter()
+        filter_file.set_name("Arquivos *.db")
+        filter_file.add_pattern("*.db")
+        dialog.add_filter(filter_file)
 
-    dialog.destroy()
+    def on_btn_fechar_clicked(self, button):
+        Gtk.main_quit()
 
+    def on_window_main_destroy(self, widget):
+        Gtk.main_quit()
 
-def add_filters(dialog):
-    filter_file = Gtk.FileFilter()
-    filter_file.set_name("Arquivos *.db")
-    filter_file.add_pattern("*.db")
-    dialog.add_filter(filter_file)
-
-
-Handlers = {
-    "on_btn_fechar_clicked": Gtk.main_quit,
-    "on_window_main_destroy": Gtk.main_quit,
-    "on_btn_file_clicked": open_file
-}
 
 if __name__ == "__main__":
     Main()
