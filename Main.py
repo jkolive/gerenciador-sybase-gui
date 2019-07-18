940014854943f914d58152d9246aca3bdc20ae8d
Jackson de Oliveira <mr.jkolive@gmail.com>
2019-07-17 22:39:19 -0300

    Lógica dos botões

--- Main.py
+++ Main.py
@@ -19,8 +19,24 @@ class Main(Gtk.Window):
         self.list_store.append([False, self.name_dir, self.name_file])
         self.treeview = builder.get_object('treeView')
 
-        btn_file = Gtk.Button()
-        btn_file.connect("clicked", self.on_btn_file_clicked)
+        self.txt_nome_servidor = builder.get_object('txt_nome_servidor')
+        self.txt_nome_servidor.set_placeholder_text('Ex. srvlinux, srvcontabil')
+
+        self.txt_mem_cache = builder.get_object('txt_mem_cache')
+        self.txt_mem_cache.set_placeholder_text('Informe em MB')
+
+        self.txt_param_rede = builder.get_object('txt_param_rede')
+        self.txt_param_servidor = builder.get_object('txt_param_servidor')
+        self.btn_gravar = builder.get_object('btn_gravar')
+        self.btn_incluir = builder.get_object('btn_incluir')
+        self.btn_iniciar = builder.get_object('btn_iniciar')
+        self.btn_parar = builder.get_object('btn_parar')
+        self.btn_parar.set_sensitive(False)
+
+        self.btn_iniciar.connect('clicked', self.on_btn_iniciar_clicked)
+        self.btn_parar.connect('clicked', self.on_btn_parar_clicked)
+        btn_incluir = Gtk.Button()
+        btn_incluir.connect("clicked", self.on_btn_incluir_clicked)
 
         renderer_toggle = Gtk.CellRendererToggle()
         renderer_toggle.connect("toggled", self.on_cbox_db_toggled)
@@ -43,7 +59,7 @@ class Main(Gtk.Window):
     def on_cbox_db_toggled(self, widget, path):
         self.list_store[path][0] = not self.list_store[path][0]
 
-    def on_btn_file_clicked(self, button):
+    def on_btn_incluir_clicked(self, button):
         dialog = Gtk.FileChooserDialog("Localize o banco de dados", self, Gtk.FileChooserAction.OPEN,
                                        (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
 
@@ -87,8 +103,50 @@ class Main(Gtk.Window):
             self.list_store.set_value(self.list_store[0].iter, 1, '')
             self.list_store.set_value(self.list_store[0].iter, 2, '')
 
+            self.txt_nome_servidor.set_sensitive(True)
+            self.txt_mem_cache.set_sensitive(True)
+            self.btn_incluir.set_sensitive(True)
+            self.btn_gravar.set_sensitive(True)
+
+    def on_btn_iniciar_clicked(self, button):
+        if not self.btn_gravar.get_sensitive():
+            self.btn_parar.set_sensitive(True)
+            self.btn_iniciar.set_sensitive(False)
+        else:
+            pass
+
+    def on_btn_parar_clicked(self, buttoon):
+        self.btn_iniciar.set_sensitive(True)
+        self.btn_parar.set_sensitive(False)
+        self.txt_nome_servidor.set_sensitive(True)
+        self.txt_mem_cache.set_sensitive(True)
+        self.btn_gravar.set_sensitive(True)
+
+    def on_btn_gravar_clicked(self, button):
+        if self.txt_nome_servidor.get_text() == '' \
+                or self.txt_mem_cache.get_text() == '' \
+                or self.list_store[self.list_store[0].iter][1] == '' \
+                or self.list_store[self.list_store[0].iter][2] == '':
+
+            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,
+                                       Gtk.ButtonsType.OK, 'INFORMAÇÃO IMPORTANTE')
+            dialog.format_secondary_text(
+                'Campos necessários ainda não preenchidos!')
+            dialog.run()
+
+            dialog.destroy()
+        else:
+            self.txt_nome_servidor.set_sensitive(False)
+            self.txt_mem_cache.set_sensitive(False)
+            self.btn_gravar.set_sensitive(False)
+
     def on_btn_fechar_clicked(self, button):
-        Gtk.main_quit(self)
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,
+                                   Gtk.ButtonsType.OK, "INFORMAÇÃO IMPORTANTE")
+        dialog.format_secondary_text("O sistema ficará executando em segundo plano.")
+        dialog.run()
+
+        dialog.destroy()
 
     def on_window_main_destroy(self, widget):
         Gtk.main_quit(self)
